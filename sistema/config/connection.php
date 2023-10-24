3dd0f7db91d5869f5133432841f248c439da0165
Gabriel Carvalho <gabriel.carvalho.pg@gmail.com>
2023-10-24 09:01:07 -0300

    alteracao do php

--- sistema/config/connection.php
+++ sistema/config/connection.php
@@ -1,26 +1,7 @@
 <?php
 $servername = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "ezpets_db";
-
-try {
-$conn = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-//echo "<h1>Conexao estabelecida!</h1>";
-
-}catch(PDOException $e){
-    echo "Erro de Conexão: A conexão falhou: ". $e->getMessage();
-}catch(Exception $e){
-    echo "Erro genérico: ". $e->getMessage();
-}
-
-?>
-<?php
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
+$username = "ezpets_admin";
+$password = "ezpets@admin";
 $dbname = "ezpets_db";
 
 try {
